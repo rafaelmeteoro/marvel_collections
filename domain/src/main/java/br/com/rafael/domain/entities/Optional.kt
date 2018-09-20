214dfa4307783c9b84af15ec2a73754c3fb3ad5b
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-09-20 20:39:49 -0300

    create module domain and package entity

---
+++ domain/src/main/java/br/com/rafael/domain/entities/Optional.kt
@@ -0,0 +1,18 @@
+package br.com.rafael.domain.entities
+
+class Optional<out T>(val value: T? = null) {
+
+    companion object {
+        fun <T> of(value: T?): Optional<T> {
+            return Optional(value)
+        }
+
+        fun <T> empty(): Optional<T> {
+            return Optional()
+        }
+    }
+
+    fun hasValue(): Boolean {
+        return value != null
+    }
+}
---
+++ domain/src/main/java/br/com/rafael/domain/entities/ThumbnailEntity.kt
@@ -0,0 +1,6 @@
+package br.com.rafael.domain.entities
+
+data class ThumbnailEntity(
+        var path: String,
+        var extension: String
+)
