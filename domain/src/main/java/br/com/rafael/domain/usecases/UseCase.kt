9e4abad42bf8cd71fa29a649b428e91b43425d42
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-09-21 10:03:07 -0300

    create abstract usecase

---
+++ domain/src/main/java/br/com/rafael/domain/usecases/UseCase.kt
@@ -0,0 +1,13 @@
+package br.com.rafael.domain.usecases
+
+import br.com.rafael.domain.common.Transformer
+import io.reactivex.Observable
+
+abstract class UseCase<T>(private val transformer: Transformer<T>) {
+
+    abstract fun createObservable(data: Map<String, Any>? = null): Observable<T>
+
+    fun observable(withData: Map<String, Any>? = null): Observable<T> {
+        return createObservable(withData).compose(transformer)
+    }
+}
