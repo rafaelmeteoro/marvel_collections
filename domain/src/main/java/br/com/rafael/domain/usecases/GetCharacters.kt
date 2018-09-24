47e2fc02f569a24e5b5259545c9c6e1646dfc721
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-09-24 15:16:45 -0300

    add dependencies

--- domain/src/main/java/br/com/rafael/domain/usecases/GetCharacters.kt
+++ domain/src/main/java/br/com/rafael/domain/usecases/GetCharacters.kt
@@ -2,15 +2,16 @@ package br.com.rafael.domain.usecases
 
 import br.com.rafael.domain.CharactersRepository
 import br.com.rafael.domain.common.Transformer
+import br.com.rafael.domain.entities.CharacterEntity
 import br.com.rafael.domain.entities.ResultEntity
 import io.reactivex.Observable
 
 class GetCharacters(
-        private val transformer: Transformer<ResultEntity>,
+        private val transformer: Transformer<List<CharacterEntity>>,
         private val charactersRepository: CharactersRepository
-) : UseCase<ResultEntity>(transformer) {
+) : UseCase<List<CharacterEntity>>(transformer) {
 
-    override fun createObservable(data: Map<String, Any>?): Observable<ResultEntity> {
-        return charactersRepository.getCharacters()
+    override fun createObservable(data: Map<String, Any>?): Observable<List<CharacterEntity>> {
+        return charactersRepository.getCharacters(0L, "", "")
     }
 }
