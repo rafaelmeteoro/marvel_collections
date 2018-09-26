881eb5942d76057298193683f28a3d57e4971d16
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-09-26 18:01:07 -0300

    get character detail module domain

--- domain/src/main/java/br/com/rafael/domain/CharactersRepository.kt
+++ domain/src/main/java/br/com/rafael/domain/CharactersRepository.kt
@@ -1,8 +1,10 @@
 package br.com.rafael.domain
 
 import br.com.rafael.domain.entities.CharacterEntity
+import br.com.rafael.domain.entities.Optional
 import io.reactivex.Observable
 
 interface CharactersRepository {
     fun getCharacters(): Observable<List<CharacterEntity>>
+    fun getCharacterDetail(characterId: Int): Observable<Optional<CharacterEntity>>
 }
---
+++ domain/src/main/java/br/com/rafael/domain/usecases/GetCharacterDetail.kt
@@ -0,0 +1,30 @@
+package br.com.rafael.domain.usecases
+
+import br.com.rafael.domain.CharactersRepository
+import br.com.rafael.domain.common.Transformer
+import br.com.rafael.domain.entities.CharacterEntity
+import br.com.rafael.domain.entities.Optional
+import io.reactivex.Observable
+
+class GetCharacterDetail(
+        transformer: Transformer<Optional<CharacterEntity>>,
+        private val charactersRepository: CharactersRepository
+) : UseCase<Optional<CharacterEntity>>(transformer) {
+
+    companion object {
+        private const val PARAM_CHARACTER_ENTITY = "param:characterEntity"
+    }
+
+    fun getById(characterId: Int): Observable<Optional<CharacterEntity>> {
+        val data = HashMap<String, Int>()
+        data[PARAM_CHARACTER_ENTITY] = characterId
+        return observable(data)
+    }
+
+    override fun createObservable(data: Map<String, Any>?): Observable<Optional<CharacterEntity>> {
+        val characterId = data?.get(PARAM_CHARACTER_ENTITY)
+        characterId?.let {
+            return charactersRepository.getCharacterDetail(it as Int)
+        } ?: return Observable.error({ IllegalArgumentException("CharacterId must be provided.") })
+    }
+}
