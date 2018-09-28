b832dd42778b032c88efb7a22a8fdcaf33f9f5a1
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-09-28 16:55:24 -0300

    create data persist

---
+++ data/src/main/java/br/com/rafael/data/entities/CharacterDataPersist.kt
@@ -0,0 +1,15 @@
+package br.com.rafael.data.entities
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "character")
+data class CharacterDataPersist(
+        @PrimaryKey
+        var id: Int,
+        var name: String,
+        var description: String? = null,
+        var modified: String,
+        var path: String?,
+        var extension: String?
+)
